bd438eb8b00cedd5629df4c664ad419fee0f11fe
ahnaf <aahnaf608@gmail.com>
2025-03-16 14:15:43 +0800

    git posttest2

---
+++ kelas/Peretemuan2/array.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    // ARRAY 1 DIMENSI 
+//     int array[3] = {1, 2, 4};
+//     // int totalbyte= sizeof(array);
+//     // int singlebyte= sizeof(array[0]);
+//     int panjangarray= sizeof(array)/sizeof(array[0]);
+//     // int panjangarray2= size(array);
+
+//     // cout<<totalbyte<<endl;
+//     // cout<<singlebyte<<endl;
+//     // cout<<panjangarray<<endl;
+//     // cout<<panjangarray2<<endl;
+
+//     // cout<<"versi c++ saya"<<__cplusplus<<endl;
+
+//     // looping array
+
+//     for (int i = 0; i<panjangarray; i++)
+//     {
+//         cout<<"nilai array: "<<array[i]<<endl;
+//     }
+//     for (int i :array)
+//     {
+//         cout<<"nilai array : "<<i<<endl;
+//     }
+
+//     // konsep crud mpada cpp
+
+//     array[2]=3;
+//     cout<<array[2]<<endl;
+
+//     array[2]=4;
+    
+//     cout<<array[2]<<endl;
+//     // menghapus data pada sebuah array
+
+//     array[2]=0;
+//     // untuk mengganti dan mengisi nilaoi yang kosng
+// for (int i = 1; i <3; i++)
+// {
+//     array[i]=array[i++];
+// }
+
+// // menampilkan array yang telah di ubah
+// for (int i : array)
+// {
+//     cout<<"nilai array: "<< i<<endl;
+// }
+
+
+// for (auto i = 0; i <2; i++)
+// {
+//     cout <<array[i]<<endl;
+// }
+
+
+
+    return 0;
+}
